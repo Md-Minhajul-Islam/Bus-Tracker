d8a8450ede57f250c1efa1bab6f4c6270e7a712c
Minhaj <minhajulislammuhammad@gmail.com>
2025-12-31 22:49:47 +0600

    test

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,7 +1,17 @@
-export const API_END_POINT = "http://localhost:5000";
-export const USER_API_END_POINT = "http://localhost:5000/user";
-export const LOCATION_API_END_POINT = "http://localhost:5000/location";
-export const MESSAGE_API_END_POINT = "http://localhost:5000/message";
+// export const API_END_POINT = "http://localhost:5000";
+// export const USER_API_END_POINT = "http://localhost:5000/user";
+// export const LOCATION_API_END_POINT = "http://localhost:5000/location";
+// export const MESSAGE_API_END_POINT = "http://localhost:5000/message";
 
+// export const ADMIN_API_END_POINT = "http://localhost:5000/admin";
 
-export const ADMIN_API_END_POINT = "http://localhost:5000/admin";
+export const API_END_POINT = "https://bus-tracker-server-gemy.onrender.com";
+export const USER_API_END_POINT =
+  "https://bus-tracker-server-gemy.onrender.com/user";
+export const LOCATION_API_END_POINT =
+  "https://bus-tracker-server-gemy.onrender.com/location";
+export const MESSAGE_API_END_POINT =
+  "https://bus-tracker-server-gemy.onrender.com/message";
+
+export const ADMIN_API_END_POINT =
+  "https://bus-tracker-server-gemy.onrender.com/admin";
